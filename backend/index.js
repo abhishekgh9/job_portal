b7aa91e8f4b2e1fb6967ef696f73c850303254ba
Abhishek Kumar <abhishekk57078@gmail.com>
2025-08-16 18:49:20 +0530

    production ready

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 const app = express();
 
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -33,6 +35,13 @@ app.use("/api/v1/application", applicationRoute);
 // "http://localhost:8000/api/v1/user/register"
 // "http://localhost:8000/api/v1/user/login"
 // "http://localhost:8000/api/v1/user/profile/update"
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get(/.*/, (_, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
